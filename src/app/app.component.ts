34bc668afd85afcb8cbc8f6501528ece56940c83
Biel <bielsesa@gmail.com>
2022-08-12 19:25:20 +0200

    ajskdjaskd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+export class AppComponent implements OnInit {
+  public monedas = [
+    { tipo: '50 cts', cantidad: 0, valor: 0.5 },
+    { tipo: '20 cts', cantidad: 0, valor: 0.2 },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+
+  public total = 0;
+
+  constructor(public storageService: StorageService) {}
+
+  async ngOnInit() {
+    // If using a custom driver:
+    // await this.storage.defineDriver(MyCustomDriver)
+    // await this.storage.create();
+    this.monedas = (await this.storageService.getObject('monedas')) ?? [
+      { tipo: '50 cts', cantidad: 0, valor: 0.5 },
+      { tipo: '20 cts', cantidad: 0, valor: 0.2 },
+    ];
+    // this.monedas = await this.storageService.getObject('total') ?? 0;
+    this.calculateTotal();
+  }
+
+  onAddMonedita(event, tipoMoneda) {
+    // check which type has changed
+    this.monedas.forEach((moneda) => {
+      if (moneda.tipo === tipoMoneda) {
+        // add one to its value
+        moneda.cantidad++;
+
+        // update total
+        this.total += moneda.valor;
+
+        this.storageService.setObject('monedas', this.monedas);
+      }
+    });
+  }
+
+  onSubtractMonedita(event, tipoMoneda) {
+    // check which type has changed
+    this.monedas.forEach((moneda) => {
+      if (moneda.tipo === tipoMoneda && moneda.cantidad > 0) {
+        // subtract one to its value
+        moneda.cantidad--;
+
+        // update total
+        this.total -= moneda.valor;
+
+        this.storageService.setObject('monedas', this.monedas);
+      }
+    });
+  }
+
+  onCantidadChanged(event, tipoMoneda) {
+    // console.log(`Event:`);
+    // console.log(event.detail.value);
+    // console.log(`Tipo moneda: ${tipoMoneda}`);
+
+    this.monedas.forEach((moneda) => {
+      if (moneda.tipo === tipoMoneda) {
+        // add one to its value
+        moneda.cantidad = event.detail.value;
+      }
+    });
+
+    this.calculateTotal();
+
+    this.storageService.setObject('monedas', this.monedas);
+  }
+
+  calculateTotal() {
+    this.total = this.monedas.reduce(
+      (sum, current) => sum + current.cantidad * current.valor,
+      0
+    );
+  }
 }
